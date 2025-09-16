abcbdd9db1b46d21aedf582c81199793923cec89
Janice <janice.bessard.1@ens.etsmtl.ca>
2025-09-15 20:22:06 -0400

    Ajout : Tests pour la fonctionnalité Redémarrer (étape 3 du tuto)

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,29 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import app from '../../src/app';
+import supertest from 'supertest'
+import { jeuRoutes } from '../../src/routes/jeuRouter';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+const testNom1 = 'Luffy';
+const testNom2 = 'Zoro'
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send({nom: testNom1})
+    await request.post('/api/v1/jeu/demarrerJeu').send({nom: testNom2})
   });
+  
+  it("devrair redémarrer le jeu", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu')
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  it("ne devrait plus y avoir de joueurs après le redémarrage", async() => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    const joueur = JSON.parse(jeuRoutes.controleurJeu.joueurs);
+    expect(joueur).toEqual([]);
+  })
 });
